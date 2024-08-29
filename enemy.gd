d3c6010cac602dab4966d60992283829b78b55a0
wei <qqwei1123@163.com>
2024-08-29 17:07:39 +0800

    add enemys

--- enemy.gd
+++ enemy.gd
@@ -2,15 +2,20 @@ extends CharacterBody2D
 
 var player: CharacterBody2D = null
 
+var health = 100
+
 @export var SPEED = 40.0
 var move = Vector2.ZERO
-
-
+var player_inattack_zone = false
+var damaged_cooldown = true
 func _physics_process(delta: float) -> void:
 	if player != null:
 		move = position.direction_to(player.position) * SPEED * delta
 		move_and_collide(move)
+		deal_on_damage()
 
+func enemy():
+	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "hero":
@@ -21,3 +26,28 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "hero":
 		player = null
 		move = Vector2.ZERO
+
+func _on_e_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+		$damaged_timer.start()
+
+
+func _on_e_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+		$damaged_timer.stop()
+
+func deal_on_damage():
+	if player_inattack_zone and gs.player_current_damage:
+		if damaged_cooldown:
+			health = health - 20
+			print(health)
+			damaged_cooldown = false
+			
+	if health <= 0:
+		self.queue_free()
+
+
+func _on_damage_timer_timeout() -> void:
+	damaged_cooldown = true
--- player2.gd
+++ player2.gd
@@ -1,27 +1,81 @@
 extends CharacterBody2D
 
+const SPEED = 5000.0
 
-const SPEED = 60.0
+var health = 100
+
+var enemy_inattack_range = false
 @onready var animation_tree: AnimationTree = $AnimationTree
-var input_direction = Vector2(0, 1)
+
+var is_attacking := false
+var is_idle := true
+var is_moving := false
+var is_dead := false
+var damaged_cooldown := true
+var input_direction = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	move()
+	move(delta)
+	attack()
+	enemy_attack()
 
-
-func move() -> void:
+func update_direction() -> Vector2:
 	if input_direction.y == 0:
 		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	if input_direction.x == 0:
 		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	velocity=input_direction * SPEED
-	move_and_slide()
-	
+	return input_direction
+
+func move(delta: float) -> void:
+	input_direction = update_direction()
 	if input_direction == Vector2.ZERO:
-		animation_tree["parameters/conditions/is_idle"] = true
-		animation_tree["parameters/conditions/is_moving"]= false
-	else :
-		animation_tree["parameters/conditions/is_idle"] = false
-		animation_tree["parameters/conditions/is_moving"]= true
+		is_idle = true
+		is_moving = false
+		velocity = Vector2.ZERO
+	elif is_attacking == false:
+		velocity = input_direction * SPEED * delta
 		animation_tree.set("parameters/idle/blend_position", input_direction)
 		animation_tree.set("parameters/move/blend_position", input_direction)
+		animation_tree.set("parameters/attack/blend_position", input_direction)
+		is_idle = false
+		is_moving = true
+		move_and_slide()
+	else:
+		animation_tree.set("parameters/attack/blend_position", input_direction)
+
+
+func _on_p_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+		$damaged_timer.start()
+
+		
+func _on_p_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+		$damaged_timer.stop()
+
+func player():
+	pass
+
+func attack():
+	if Input.is_action_pressed("attack"):
+		is_attacking = true
+		gs.player_current_damage = true
+	else:
+		is_attacking = false
+		gs.player_current_damage = false
+		
+func enemy_attack():
+	if enemy_inattack_range and damaged_cooldown:
+		health -= 10
+		damaged_cooldown = false
+		print("player:", health)
+		if health <= 0:
+			is_dead = true
+			enemy_inattack_range = false
+			print("enemy killed")
+
+
+func _on_damage_timer_timeout() -> void:
+	damaged_cooldown = true;
